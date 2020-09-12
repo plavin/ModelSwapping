6dbf852a87317051e82130cce1cbdb224e016421
Patrick Lavin <plavin997@gmail.com>
2020-09-12 13:43:10 -0400

    remove meabo

--- trace-generation/meabo/meabo_medium.sh
+++
@@ -1,3 +0,0 @@
-# This is the run used for my phase analysis work.
-N=512
-./meabo.x86_64 -i 125 -s $N -r $N -c $N -R $N -C 1 -T 1 -P521 -L2
--- trace-generation/meabo/phase_boundary.h
+++
@@ -1,4 +0,0 @@
-#ifndef PHASE_BOUNDARY_H
-#define PHASE_BOUNDARY_H
-void __attribute__ ((noinline)) phase_boundary8  (long *arr, long arr_len, long val, long iter);
-#endif
